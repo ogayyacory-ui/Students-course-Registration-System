c1eb40871c688c81fae60b57fa7c905f00a8a6f6
cory <cory.martel@student.moringaschool.com>
2026-06-09 15:50:07 +0300

    created classes for course and student

---
+++ models/course.py
@@ -0,0 +1,6 @@
+class Course:
+    def __init__(self, course_id, course_name, trainer_name, capacity):
+        self.course_id = course_id
+        self.course_name = course_name
+        self.trainer_name = trainer_name
+        self.capacity = capacity    
---
+++ models/student.py
@@ -0,0 +1,6 @@
+class student :
+    def __init__(self, student_id, name, email, phone_number):
+        self.student_id = student_id
+        self.name = name
+        self.email = email
+        self.phone_number = phone_number    
